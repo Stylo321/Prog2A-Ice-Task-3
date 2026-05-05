dbeeee74031e6d9c141403dfa1dc0ef3c61945b8
Stylo <165194260+Stylo321@users.noreply.github.com>
2026-05-05 18:19:21 +0200

    Add files via upload

---
+++ EventsActivity/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Alarm
+{
+    // Task 1: Delegate
+    public delegate void AlarmHandler();
+
+    // Task 2: Event
+    public event AlarmHandler OnAlarmTriggered;
+
+    // Method to trigger the alarm
+    public void TriggerAlarm()
+    {
+        Console.WriteLine("Alarm is triggered!");
+
+        // Invoke the event (only if there are subscribers)
+        OnAlarmTriggered?.Invoke();
+    }
+}
+
+class Program
+{
+    // Task 3: Method to respond to alarm
+    public static void RespondToAlarm()
+    {
+        Console.WriteLine("Warning! Take action immediately!");
+    }
+
+    static void Main(string[] args)
+    {
+        // Task 4: Create Alarm object
+        Alarm alarm = new Alarm();
+
+        // Subscribe method to event
+        alarm.OnAlarmTriggered += RespondToAlarm;
+
+        // Trigger the alarm
+        alarm.TriggerAlarm();
+    }
+}
